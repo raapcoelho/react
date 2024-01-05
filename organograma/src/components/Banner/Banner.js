5fffb8faaeed9f1da4dad3ac68d8496775e621a3
raapcoelho <49928252+raapcoelho@users.noreply.github.com>
2024-01-04 21:10:22 -0300

    Primeiro código e limpando o repositório

---
+++ organograma/src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+function Banner() {
+    // JSX - Ele enentede o que é e faz um appende no DOM
+    // Ou seja, não é HTML, é como o react lê e transforma em elementos no DOM
+    // Parece HTML mas não é
+    return (
+        <header className="banner">
+            <img src="/images/banner.jpg" alt="Janis Joplin"/>
+        </header>
+    )
+}
+
+export default Banner
