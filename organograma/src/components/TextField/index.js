8f54a518bbb5e34d1c4dcadc230af2b7b368ae2d
raapcoelho <49928252+raapcoelho@users.noreply.github.com>
2024-01-08 21:09:01 -0300

    Interagindo com o usuário

--- organograma/src/components/TextField/index.js
+++ organograma/src/components/TextField/index.js
@@ -1,11 +1,18 @@
 import './TextField.css'
 // Aero Function
 // o props é passado pelo react de forma implicita, que são as propriedades que o componente recebeu
+
+let value = ''
+const keyPress = (event) =>{
+    value = event.target.value
+    console.log(value);
+}
+
 const TextField = (props) =>{
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input required={props.required} placeholder={props.placeholder}/>
+            <input value={value} onChange={keyPress} required={props.required} placeholder={props.placeholder}/>
         </div>
     )
 }
