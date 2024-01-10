8f2e94643c5341094f4d947d4514e29bc36f8304
raapcoelho <49928252+raapcoelho@users.noreply.github.com>
2024-01-10 20:34:06 -0300

    Listando itens

--- organograma/src/App.js
+++ organograma/src/App.js
@@ -1,9 +1,47 @@
 import { useState } from 'react';
 import Banner from './components/Banner'
 import Form from './components/Form'
+import Team from './components/Team'
 
 function App() {
 
+  const teams = [
+    {
+      name: 'Programação',
+      primaryColor: '#57C278',
+      secondaryColor: '#D9F7E9'
+    },
+    {
+      name: 'Front-end',
+      primaryColor: '#82CFFA',
+      secondaryColor: '#E0F6FF'
+    },
+    {
+      name: 'Data Science',
+      primaryColor: '#A6D157',
+      secondaryColor: '#F0F8E2'
+    },
+    {
+      name: 'Devops',
+      primaryColor: '#E06B69',
+      secondaryColor: '#FDE7E8'
+    },
+    {
+      name: 'UX e Design',
+      primaryColor: '#DB6EBF',
+      secondaryColor: '#FAE9F5'
+    },
+    {
+      name: 'Mobile',
+      primaryColor: '#FFBA05',
+      secondaryColor: '#FFF5D9'
+    },
+    {
+      name: 'Inovação e Gestão',
+      primaryColor: '#FF8A29',
+      secondaryColor: '#FFEEDF'
+    },
+  ]
   const [tickets, setTickets] = useState([])
 
   const addTicket = (ticket) => {
@@ -14,7 +52,10 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Form saveTicket={ticket => addTicket(ticket)}/>      
+      <Form saveTicket={ticket => addTicket(ticket)}/>   
+
+      {teams.map(team => <Team key={team.name} name={team.name} primaryColor={team.primaryColor} secondaryColor={team.secondaryColor}/>)}
+
     </div>
   );
 }
